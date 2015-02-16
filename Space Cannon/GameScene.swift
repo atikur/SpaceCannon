8b2f4a296b01e4fbf6cc8d5aff2c33b3b43c445d
Atikur Rahman <atik.bd08@gmail.com>
2015-02-16 10:39:01 +0600

    add rotating cannon

--- Space Cannon/GameScene.swift
+++ Space Cannon/GameScene.swift
@@ -9,18 +9,38 @@
 import SpriteKit
 
 class GameScene: SKScene {
+    var mainLayer: SKNode!
+    var cannon: SKSpriteNode!
+    
     override func didMoveToView(view: SKView) {
-
+        let background = SKSpriteNode(imageNamed: "StarField")
+        background.position = CGPointMake(size.width/2, size.height/2)
+        background.blendMode = SKBlendMode.Replace
+        self.addChild(background)
+        
+        // add main layer
+        mainLayer = SKNode()
+        self.addChild(mainLayer)
+        
+        // add cannon
+        cannon = SKSpriteNode(imageNamed: "Cannon")
+        cannon.position = CGPointMake(size.width/2, 0)
+        mainLayer.addChild(cannon)
+        
+        // rotate cannon
+        let halfRotateAction = SKAction.rotateByAngle(CGFloat(M_PI), duration: 2)
+        let rotateAction = SKAction.sequence([halfRotateAction, halfRotateAction.reversedAction()])
+        cannon.runAction(SKAction.repeatActionForever(rotateAction))
     }
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         /* Called when a touch begins */
         
         for touch: AnyObject in touches {
-
+            
         }
     }
-   
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
